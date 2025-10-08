fa0e6f522039f7d3695e63b54b231e6a1adf4481
dinosauce-285 <thanhbualqt@gmail.com>
2025-10-08 14:00:16 +0700

    done banner 1

--- frontend/src/components/logo.jsx
+++ frontend/src/components/logo.jsx
@@ -1,8 +1,8 @@
 function Logo({className}) {
     return (
         <div className={`logo h-[100%] flex flex-row justify-center items-center ${className}`}>
-            <img src="./logo.png" alt="" className="h-[100%] object-contain" />
-            <h3 style={{ textShadow: "2px 2px 4px rgba(0,0,0,0.5)" }} className="font-poppins font-semibold">Granny's Secret</h3>
+            <img src="./logo.png" alt="" className="h-[80%] object-contain " />
+            <div style={{ textShadow: "2px 2px 4px rgba(0,0,0,0.5)" }} className="font-poppins font-semibold ml-5">Granny's Secret</div>
         </div>
     )
 }
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -11,10 +11,17 @@ function LandingPage()
                     <Button1 className="w-28">Sign up</Button1>
                 </div>
             </div>
-            <div className="banner h-screen flex flex-row">
-                <div className="text h-[100%] flex flex-col w-1/2">
-                    <div className="boldText h-[20%] bg-amber-500 w-[100%]"></div>
-                    <div className="smallText flex-1 bg-green-500 w-[100%]"></div>
+            <div className="banner h-screen flex flex-row items-center mt-20 ml-5">
+                <div className="text flex flex-col w-1/2 ">
+                    <div className="boldText  w-[100%] text-[5.4rem] font-bold font-poppins text-justify">
+                        Every meal tells  
+                    </div>
+                    <div className="boldText  w-[100%] text-[5.5rem] font-bold font-poppins">
+                        a memory
+                    </div>
+                    <div className="smallText flex-1 w-[100%] text-justify font-inter text-2xl">
+                        Cooking is more than just preparing food — it is a way of keeping memories alive. Every recipe carries a story, whether it is a dish passed down through generations or something new you discovered yesterday. With our platform, you can safely store, organize, and revisit all your favorite recipes anytime. No matter where you are, your kitchen memories will always be within reach, ready to inspire your next meal. This space is designed to celebrate tradition, creativity, and the joy of sharing food with the people you love.
+                    </div>
                 </div>
                 <div className="bannerImg w-1/2">
                     <img src="./LandingPagePic1.png" alt="" />
